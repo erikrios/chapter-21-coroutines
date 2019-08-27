dd44ae9a030751e90c4e35f9ade2336dd2d89655
erikrios <erikriosetiawan49@gmail.com>
2019-08-27 21:18:33 +0700

    Initial commit

---
+++ src/main/kotlin/com/erikriosetiawan/Main.kt
@@ -0,0 +1,17 @@
+package com.erikriosetiawan
+
+import javafx.application.Application.launch
+
+//fun main() {
+//    launch(CommonPool) {
+//        delay(1000L)
+//        println("World!")
+//    }
+//
+//    println("Hello,")
+//    Thread.sleep(2000L)
+//}
+//
+//fun launch(commonPool: Any, any: Any) {
+//
+//}
